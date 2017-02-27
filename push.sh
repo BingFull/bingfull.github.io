65f0b4d1ff6e1237b0a001b8ae8f43222f8f34e5
BingFull <453476815@qq.com>
2017-02-27 22:06:23 +0800

    Fixed some bugs

--- push.sh
+++ push.sh
@@ -4,7 +4,7 @@ cd ./public
 git init
 git config --global push.default matching
 git config --global user.email "453476815@qq.com" 
-git config --global user.name "BingFullGit" 
+git config --global user.name "BingFull" 
 git add --all .
 git commit -m "Travis CI Auto Builder"
 git push --quiet --force https://${Hexo_TOKEN}@https://github.com/BingFull/bingfull.github.io.git master
